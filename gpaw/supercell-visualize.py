b1b15d8ac5f917485e059d487d77215cba293894
Jack Crewse <jcrewseiii@gmail.com>
2022-11-02 14:47:26 -0500

    More flexiblility to explore configs.

--- gpaw/supercell-visualize.py
+++ gpaw/supercell-visualize.py
@@ -3,15 +3,34 @@ from ase import io
 from ase.visualize import view
 from math import sqrt
 
+# hBN lattice parameters
 a = 1.42
-c = 3.28
+c = 6.709
+
+# Twist angle (in degrees)
+phi = 10.0
+
+# Number of layers in structure
+N_layers = 2
+
+layer_angles = []
+
+# hBN is isotructural to graphene. Create graphene like lattice.
 graphene = sc.lattice()
 graphene.set_vectors([3*a/2, a*sqrt(3)/2, 0], [3*a/2, -a*sqrt(3)/2, 0], [0, 0, c])
 graphene.add_atom("B", (0, 0, 0)).add_atom("N", (a, 0, 0))
 
-structure = sc.heterostructure().set_substrate(graphene).add_layer(graphene)
+# Begin creating the structure
+# Create a substrate (first layer)
+structure = sc.heterostructure().set_substrate(graphene)
 
-res = structure.opt(thetas=[10*sc.DEGREE], log = True)
+# Add layers on top of substrate
+for n in range(N_layers-1):
+    structure.add_layer(graphene)
+    layer_angle = (n+1)*phi
+    layer_angles.append([layer_angle*sc.DEGREE])
+
+res = structure.opt(max_el = 6, thetas=layer_angles, log = True)
 print(res.log)
 
 res = structure.calc(M = res.M(), thetas = res.thetas())
