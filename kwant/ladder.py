a8ade7c8a5b627251db89b92ccaa649f3a41ac14
Jack Crewse <jcrewseiii@gmail.com>
2023-02-20 10:29:00 -0600

    git update. ladder changes. gitignore changes.

--- kwant/ladder.py
+++ kwant/ladder.py
@@ -13,17 +13,15 @@ def main():
 
     os.system('clear')
 
-    #analyze_bandstructure_varyt()
-    #analyze_bandstructure_varyv()
-    #analyze_bandstructure_varyw()
-    #analyze_bandstructure_varyphi()
+    width = 2
+    cell_scale = 2
 
     # Create a finite system to visualize the structure
     system = make_system(a = 1, b = 1, phi = 0, 
-                         u = 5, hops = (5,1,1), 
-                         width = 2, cell_scale = 2).finalized()
+                         u = 1, hops = (5,2,1), 
+                         width = width, cell_scale = cell_scale).finalized()
 
-    plot_band_state(system)
+    plot_band_state(system, width, cell_scale)
     #plot_spec_prob(system, n_eigs=1, plot_eigs=[0])
 
     return
@@ -33,7 +31,6 @@ def make_system(a, b, phi, u, hops, width, length=None, cell_scale = 1):
     t, v, w = hops
 
     lat = kwant.lattice.general([(a,0), (0,b)], norbs=1)
-
     sys = kwant.Builder()
 
     if length is not None:
@@ -42,7 +39,8 @@ def make_system(a, b, phi, u, hops, width, length=None, cell_scale = 1):
     else:
         # Create an infinite system for band structure calculations
         sys = kwant.Builder(kwant.TranslationalSymmetry((cell_scale*a,0)))
-        sys[lat.shape((lambda pos: pos[1] >= 0 and pos[1] < b*width), (0,0))] = u
+        sys[lat.shape((lambda pos: (pos[1] >= 0 and pos[1] < b*width) and (pos[0] >= 0 and pos[0] < cell_scale*a)), (0,0))] = u
+        #sys[lat.shape((lambda pos: pos[1] >= 0 and pos[1] < b*width), (0,0))] = u
 
     # Intra-layer hoppings
     sys[kwant.builder.HoppingKind((0,b), lat, lat)]  = -t
@@ -288,87 +286,126 @@ def plot_spec_prob(sys, n_eigs, plot_eigs):
 
     return       
 
-def plot_band_state(sys):
+def plot_band_state(sys, width, cell_scale):
 
-    # What states to plot, momenta to choose from. Likely function parameters soon
-    states = [0,1,2]
-    state_momenta = [0,pi/4,pi/2,3*pi/4,pi]
+    kx = np.pi - 0.01
     
-    '''Retrieve positon/wavevector parameters'''
-    # Extract site positions from system
-    # Only the first half of the sites are in the 'fundamental domain'
-    # Width is the number of y-values
-    # Momenta in the first BZ are |k| < pi/b
+    '''Calculate position dependent variables
+    ---------------------------------------------------------------------------
+    1. Create an array of all positions as returned from the system object.
+    2. Slice off all positions outside of the unit cell (fundamental domain)*
+        * kwant.system.InfiniteSystem included all sites in a unit cell plus 
+        sites in the previous cell connected by the hoppings. 
+    '''
     pos = np.array([site.pos for site in sys.sites])
-    pos = pos[:len(pos)//2]     
-    kx = state_momenta[4]
+    pos = pos[:-width]                                  
+    
 
-
-    '''Start the figure for band structure and eigenstates'''
-    # Create band structure subplots
-    # Add subplot in a 1 row, 2 column config. band_ax is axis 1 (counting from top left)
-    # Initialize color interator (different color for each plotted eigenstate)
-    fig = plt.figure(figsize=(10,int(len(states)/2)+6))
-    band_ax = fig.add_subplot(1,2,1) 
-    colors = iter(cm.rainbow(np.linspace(0,1,len(states))))
-
-
-    '''Calculate band energies and momenta'''
-    # Create a physic.Bands instance from the system
-    # Define momenta for |k| < pi
-    # Calculate eigenenergies by calling bands instance
+    '''Calculate band energies
+    ---------------------------------------------------------------------------
+    1. Create a kwant.physics.Bands object for the system
+    2. Define an array of momenta values: 0 <= k <= pi
+    3. Call Bands to return energies for each k
+    4. Calculate the number of bands (states) per k
+    '''
     bands = kwant.physics.Bands(sys)
     momenta = np.linspace(0, pi, 100)
     energies = [bands(k) for k in momenta]
+    N_states = len(bands(kx))
+    
+
+    '''Initialize figure
+    ---------------------------------------------------------------------------
+    1. Create figure object
+    2. Create subplot axis for band structure
+    3. Initialize a color iterator for each plotted eigenstate
+    '''
+    fig = plt.figure(figsize=(12,int(N_states/2)+8))
+    band_ax = fig.add_subplot(1,2,1)
+    colors = iter(cm.Set1(np.arange(N_states)))
 
 
-    '''Plot band structure on band_ax'''
+    '''Plot the band structure
+    ---------------------------------------------------------------------------
+    1. Plot E(k) vs. k
+    2-4. Set plot labels/parameters
+    '''
     band_ax.plot(momenta, energies, color = 'k')
     band_ax.set_xlim(0,pi)
     band_ax.set_ylabel(r'$\epsilon(k)$')
     band_ax.set_xlabel(r'$k$')
 
 
-    '''Plot range of eigenstates on state_ax'''
-    # Calculate eigvals/vecs from bands instance 
-    for n in states:
+    '''Plot eigenstates
+    ---------------------------------------------------------------------------
+    For each state for the given k, calculate the Bloch function.
+    1. Calculate eigenvalues (energies) and eigenvectors (states) a given k
+    2. Eigvec = u_nk for a single unit cell. Repeat N times in the x-direction
+    3. Calculate Bloch states in x-direction via modulating by plane wave.
+    '''
+    ax = []
+    for n in range(N_states):
 
         # Retrieve eigvals/vecs from bands instance
         # eigvals = E_n(k_x)
         # eigvecs = u_{n,k_x}(0,y)
         eigvals, eigvecs  = bands(kx, return_eigenvectors=True)
+        eigvals, eigvecs = sorted_eigs((eigvals, eigvecs))
+        print("Eigenvalue {}: ".format(n) + '{:3.3f}'.format(eigvals[n]))
+        print("Eigenvector {}: \n".format(n) + str(eigvecs[:,n]) + '\n')
 
         N = 10
         # Repeat u_nk in x-direction N times
         u_nk = []
-        for _ in range(N): 
-            u_nk.append(eigvecs[n,:])
+        for _ in range(N):
+            # Split u_nk array to isolate primitive unit cell
+            u = np.array_split(eigvecs[:,n], cell_scale)
+            for i in range(cell_scale):
+                u_nk.append(u[i])
         u_nk = np.array(u_nk)
-        print('u_nk shape: ' + str(np.shape(u_nk)))
+        print(np.shape(u_nk))
 
         # Calculate psi by modulating u_nk with plane wave
         psi = []
-        for xi in range(N):
-            psi.append(np.exp(1j*kx*xi)*u_nk[xi,:])
+        for ni in range(N):
+            for xi in range(cell_scale):
+                x = xi + cell_scale*ni
+                psi.append(np.exp(1j*kx*x/cell_scale)*u_nk[xi,:])
         psi = np.array(psi)
-        print(r"$\Psi$ shape: " + str(np.shape(psi)))
-
-        # Print some user output
-        print("Momenta: kx = {:3.5f}".format(kx))
+    
 
         # Iterate to next color for each plotted state
         color = next(colors)
 
+        re_psi = psi.real.transpose()
+        im_psi = psi.imag.transpose()
+        #print('Real Psi: \n' + str(re_psi))
+        #print('Imag Psi: \n' + str(im_psi))
+
         # Plot eigenstates on the state_ax
         # Mark the corresponding E_n(k) on band_ax
         # Plot \Psi_{nk}(y) on state_ax
-        state_ax = fig.add_subplot(len(states), 2, 2*len(states) - 2*n)
-        band_ax.scatter(kx, eigvals[n], color = color, zorder = 2, clip_on = False)
-        state_ax.imshow(psi.real.transpose())
-        state_ax.set_title('$\Psi_{}(x,y)$'.format(n), fontdict = {'color': color})
+        state_ax = fig.add_subplot(N_states, 2, 2*(N_states - n))
+        band_ax.scatter(kx, eigvals[n], 
+                        color = color, s = 100,
+                        zorder = 2, clip_on = False)
+        psi_plot = state_ax.imshow(re_psi, vmin = -0.5, vmax = 0.5)
+        #psi_plot = state_ax.imshow(im_psi)
+
+        # Titles, limits, labels, etc...
+        state_ax.set_title('$\Re[\Psi_{}(x,y)] \quad E_{} = {:3.2f}$'.format(n, n, eigvals[n]), fontdict = {'color': color})
         state_ax.set_xlabel('x')
         state_ax.set_ylabel('y')
         state_ax.yaxis.tick_right()
+
+        # Plot the unit cell
+        if cell_scale > 0:
+            for xi in range(N): state_ax.vlines(cell_scale*xi-0.5, -width, width, color = 'k', linestyles='dashed')
+            #state_ax.set_xlim(np.min(pos[:,0])-0.5, N*cell_scale*np.max(pos[:,0])-0.5)
+            state_ax.set_ylim(np.min(pos[:,1])-0.5, np.max(pos[:,1])+0.5)
+        ax.append(state_ax)
+
+    fig.colorbar(psi_plot, ax = ax[:], location = 'bottom')
 
     plt.show()
 
