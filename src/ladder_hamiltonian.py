32647112de839c6320349f15f348d30ac42f63b2
Jack Crewse <jcrewseiii@gmail.com>
2023-03-13 15:15:36 -0500

    Rotation of H_xy in progress.

--- src/ladder_hamiltonian.py
+++ src/ladder_hamiltonian.py
@@ -21,49 +21,55 @@ def hamiltonian(lat_const_a, lat_const_b, width, hops, phi,
         sys[lat.shape((lambda pos: (pos[1] >= 0 and pos[1] < lat_const_b*width) 
                        and (pos[0] >= 0 and pos[0] < n_phi*lat_const_a)), (0,0))] = on_site_pot
     else:
-        # System is translationally symmetric for n_phi layers
+        # System is translationally symmetric for translations
+        # T = n_phi*a
         sys = kwant.builder.Builder(kwant.lattice.TranslationalSymmetry((n_phi*lat_const_a,0)))
         
+        # Define the molecular Hamiltonian
+        # Rotation matrices
         H_molecule = [[on_site_pot, -intra_hop],[-intra_hop, on_site_pot]]
         rot_matrix = [[cos(phi), -sin(phi)],[sin(phi), cos(phi)]]
+        rot_matrix_dagger = np.transpose(rot_matrix)
         
-        for i in range(n_phi):
+        H_rotated = H_molecule
+        for n in range(n_phi):
             # Rotate the molecular Hamiltonian
-            # For each layer in the unit cell we perform the unitary transformation
+            # For each layer in unit cell, perform the unitary transformation
             # H --> RHR^+
             # This transformation is applied each layer so that after 
             # n_phi layers we have
             # H --> R^{n_phi}HR^{+ n_phi}
-            rot_matrix_dagger = np.transpose(rot_matrix)
-            H_rotated = np.matmul(H_molecule, rot_matrix_dagger)
-            H_rotated = np.matmul(rot_matrix, H_rotated)
+            print(f'Layer = {n}')
+            print(f'H_molecule = {H_molecule}')
+            # Raise rotation matrices to appropriate power
+            R  = np.linalg.matrix_power(rot_matrix, n)
+            RT = np.linalg.matrix_power(rot_matrix_dagger, n) 
             
-            sys[lat(i,0)] = H_rotated[0,0]
-            sys[lat(i,1)] = H_rotated[1,1]
+            # Carry out the unitary transformation
+            H_rotated = np.matmul(H_rotated, RT)
+            H_rotated = np.matmul(R, H_rotated)
+            print(f'H_rot = \n {H_rotated}')
+            
+            # On-site pot in each layer set to diagonal terms 
+            sys[lat(n,0)] = H_rotated[0,0]
+            sys[lat(n,1)] = H_rotated[1,1]
 
-    # Intra-layer hoppings
-    # H_12
-    H_12 = sin(phi)*(intra_hop*sin(phi) + on_site_pot*cos(phi)) \
-            - cos(phi)*(intra_hop*cos(phi) + on_site_pot*sin(phi))
-    sys[kwant.builder.HoppingKind((0,1), lat, lat)] = H_12
-            
-    # H_21
-    H_21 = cos(phi)*(on_site_pot*sin(phi) - intra_hop*cos(phi)) \
-            - sin(phi)*(on_site_pot*cos(phi) - intra_hop*sin(phi))
-    sys[kwant.builder.HoppingKind((0,-1), lat, lat)] = H_21
+            # Intra-layer hoppings in each layer set as off-diagonals
+            sys[kwant.builder.HoppingKind((n,1), lat, lat)] = H_rotated[0,1]
+            sys[kwant.builder.HoppingKind((n,-1), lat, lat)] = H_rotated[1,0]
     
     # Inter-layer hoppings
-    l_v = (0.5*lat_const_a**2)*(1-np.cos(phi)) + lat_const_b**2
+    l_v = 1#(0.5*lat_const_a**2)*(1-np.cos(phi)) + lat_const_b**2
     sys[kwant.builder.HoppingKind((1,0), lat, lat)]   = -inter_hop/l_v
 
     # Cross hoppings
-    l_u = (0.5*lat_const_a**2)*(1+np.cos(phi)) + lat_const_b**2
+    l_u = 1#(0.5*lat_const_a**2)*(1+np.cos(phi)) + lat_const_b**2
     sys[kwant.builder.HoppingKind((1,1), lat, lat)]   = -cross_hop/l_u
     sys[kwant.builder.HoppingKind((-1,1), lat, lat)]  = -cross_hop/l_u
     
     # Output
     if output:
-        np.set_printoptions(linewidth=150)
+        np.set_printoptions(linewidth=200)
         print('========== Hamiltonian initialized ==========')
         print(f'Lattice constants: a = {lat_const_a}, b = {lat_const_b}')
         print(f'Finite width = {width}')
