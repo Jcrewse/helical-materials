efbce5ba8b65a8ad14519e8247a6588de899f2b0
Jack Crewse <jcrewseiii@gmail.com>
2023-06-05 22:30:37 -0500

    parallel output

--- scripts/gpaw/analysis.py
+++ scripts/gpaw/analysis.py
@@ -1,35 +1,41 @@
 import os
 from ase.units import Ha
-from gpaw import PW, FermiDirac
 from ase.visualize import view
+from ase.parallel import world
+from gpaw import PW, FermiDirac
 from src.GPAWCalculator import *
 from src.ase_systems import chain_systems
+from src.ase_systems import bulk_systems
 from math import pi
 
 # Calculation parameters ######################################################
 # Parameters not list are GPAW calc.default_parameters
 params = {
-    'mode'        : PW(500),             # Calculation mode        
-    'kpts'        : (30,1,1),            # k-points sampled in periodic sys
+    'mode'        : PW(300),             # Calculation mode        
+    'kpts'        : (5,1,1),             # k-points sampled in periodic sys
     'random'      : True,                # Random guess of WF's in empty bands
     'xc'          : 'PBE',               # Exchange-correlation function
     'occupations' : FermiDirac(0.01),    # Occupation smearing (input # = kT)
-    'convergence' : {'energy' : 0.0005}  # Convergence criteria
+    'convergence' : {'energy' : 0.005}  # Convergence criteria
     }
 ###############################################################################
 
 # Create System ###############################################################
-system = chain_systems.CO_chain()
-#system.show()
+#system = chain_systems.CO_chain(twist_angle = pi)
+system = bulk_systems.hBN(twist_angle = 2*pi/2, max_el = 10)
+#system.show(repeat=(1,1,1))
 
 # Ground State Calculations ###################################################
-print('\n========== CALCULATING GROUND STATE ==========\n')
-calc_groundstate(system, params, restart=False)
+if world.rank == 0: 
+    print('\n========== CALCULATING GROUND STATE ==========\n')
+calc_groundstate(system, params, restart=True)
     
 # Calculate Wave Functions ####################################################
-print('\n========== CALCULATING WAVEFUNCTIONS ==========\n')
+if world.rank == 0: 
+    print('\n========== CALCULATING WAVEFUNCTIONS ==========\n')
 calc_wavefunction(system, kpt=0)
 
 # Calculate Band Structure ####################################################
-print('\n========== CALCULATING BAND STRUCTURE ==========\n')
-calc_bandstructure(system, npoints=100, unfold=True)
+if world.rank == 0: 
+    print('\n========== CALCULATING BAND STRUCTURE ==========\n')
+calc_bandstructure(system, npoints=600, unfold=True)
