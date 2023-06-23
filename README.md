dc4c41dff0756c3fa1a784b55d38a4984577e47a
Jack Crewse <jcrewseiii@gmail.com>
2023-06-23 07:44:29 -0500

    reorganizing

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Helical Materials
-Investigating nanoscale materials with helical symmetry via tight-binding and DFT simulation.
+Investigating nanoscale materials with helical symmetry via density functional theory simulation. 
+
+## Installation
+1. Close repository: `git clone https://github.com/jcrewse/helical-materials`
+2. Install dependencies: GPAW `conda install -c conda-forge gpaw`, 
+supercell-core `pip3 install supercell-core --user`
+3. Install package: `pip3 install -e .`
--- scripts/analysis.py
+++ scripts/analysis.py
@@ -1,34 +1,33 @@
-import os
+import os, time
 from ase.units import Ha
 from ase.visualize import view
 from ase.parallel import world
 from gpaw import PW, FermiDirac
 from src.GPAWCalculator import *
-from src.ase_systems import chain_systems
-from src.ase_systems import bulk_systems
+from src.ase_systems import systems
 from math import pi
 
 # Calculation parameters ######################################################
 # Parameters not list are GPAW calc.default_parameters
 params = {
-    'mode'        : PW(500),             # Calculation mode        
+    'mode'        : PW(300),             # Calculation mode        
     'kpts'        : (5,5,5),             # k-points sampled in periodic sys
     'random'      : True,                # Random guess of WF's in empty bands
     'xc'          : 'PBE',               # Exchange-correlation function
     'occupations' : FermiDirac(0.01),    # Occupation smearing (input # = kT)
-    'convergence' : {'energy' : 0.005}  # Convergence criteria
+    'convergence' : {'energy' : 0.0001}  # Convergence criteria
     }
 ###############################################################################
 
 # Create System ###############################################################
-#system = chain_systems.CO_chain(twist_angle = pi)
-system = bulk_systems.hBN(twist_angle = 2*pi/2, max_el = 6)
-system.show(repeat=(1,1,1))
+system = systems.hBN()
+#system.show(repeat=(1,1,1)) # 
 
+ti = time.perf_counter()
 # Ground State Calculations ###################################################
 if world.rank == 0: 
     print('\n========== CALCULATING GROUND STATE ==========\n')
-calc_groundstate(system, params, restart=True)
+calc_groundstate(system, params, restart=False)
     
 # Calculate Wave Functions ####################################################
 if world.rank == 0: 
@@ -38,4 +37,7 @@ calc_wavefunction(system, kpt=0)
 # Calculate Band Structure ####################################################
 if world.rank == 0: 
     print('\n========== CALCULATING BAND STRUCTURE ==========\n')
-calc_bandstructure(system, npoints=500, unfold=False)
+calc_bandstructure(system, npoints=50, unfold=False)
+
+tf = time.perf_counter()
+print(f'Time: {tf-ti}')
